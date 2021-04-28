2db42e71abba0963c6e12acfbbbcd5a6e7a96bcc
BrianPerillo <brian_perillo@hotmail.com>
2021-04-28 12:54:35 -0300

    Agrego funciones a los comps de livewire

--- app/Http/Livewire/PurchaseRaffle.php
+++ app/Http/Livewire/PurchaseRaffle.php
@@ -4,10 +4,102 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use App\Models\Draw;
+use App\Models\Raffle;
+
 class PurchaseRaffle extends Component
 {
-    public function render()
-    {
-        return view('livewire.purchase-raffle');
+    public $quantity = 1;
+    public $final_price;
+    public $message = null;
+    public $selected_raffle; // (Rifa que se pretende agregar)
+    public $check_availability;
+    public $available_raffles = []; // (Rifas Agregadas)
+    public $raffle_exist_on_array = false;
+
+    public function add_raffle(){
+
+        $this->check_availability = null;
+        $this->message = null;
+        $this->raffle_exist_on_array = false;
+        
+        foreach($this->available_raffles as $raffle){
+
+            if($raffle == $this->selected_raffle){
+                $this->raffle_exist_on_array = true;
+            }
+
+        }
+
+        //Rifa Ingresada
+            $selected_raffle = $this->selected_raffle;
+
+        //Checkeo si la rifa se encuentra disponible y sea un número válido:
+            //Busco la rifa en la DB
+            $check_availability = Raffle::where('number', '=', "$selected_raffle")->get();
+            
+            //Si no está disponible (la consulta arrojó resultados):
+            if(sizeof($check_availability) > 0){
+    
+                $this->message = "Este número de rifa no está disponible";
+
+            }
+            //Si el número de rifa esté disponible pero no sea un número válido
+            else if($selected_raffle<1){
+
+                $this->message = "Debe ingresar un número válido";
+
+            }
+            //Si la rifa ya fue agregada y está repitiendo el número:
+            else if($this->raffle_exist_on_array){
+
+                $this->message = "Esa rifa ya se encuentra agregada";
+    
+            }
+            //En caso de que esté disponible y también sea válido la agrego al array: 
+            else{
+                
+                $this->available_raffles[] = "$selected_raffle";
+    
+            }
+
+    }
+
+    public function remove_raffle(){
+
+        if(sizeof($this->available_raffles)>0){
+
+            $last_raffle_index = count($this->available_raffles) - 1;
+            unset($this->available_raffles[$last_raffle_index]);
+            
+            $this->message = "Rifa removida";
+        }
+        else{
+            $this->message = "No hay rifas para quitar" ;
+        }
+
+    }
+
+    public function render(){
+        
+        //Consulto el pozo total
+        $draw = Draw::get()->first();
+
+        $jackpot_value = $draw->jackpot_value;
+
+        //Calculo precio de las rifas (1% del jackpot/pozo acumulado) 
+        $raffles_price = $jackpot_value*0.01;
+        
+        //Calculo precio por el total de las rifas solicitadas 
+
+        if(is_numeric($this->quantity)){
+            $this->final_price = round(($this->quantity*$raffles_price), 2);
+        }
+        else{
+            $this->final_price = null;
+        }
+
+        return view('livewire.raffle-calculator', ['final_price',  "message", "available_raffles"]);
+
     }
 }
--- app/Http/Livewire/RafflesInfo.php
+++ app/Http/Livewire/RafflesInfo.php
@@ -4,10 +4,35 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use App\Models\Draw;
+
 class RafflesInfo extends Component
 {
-    public function render()
-    {
-        return view('livewire.raffles-info');
+
+    public $jackpot_value;
+    public $dollar_value;
+    public $raffles_price;
+
+    public function render(){
+
+        //Consulto el pozo total
+        $draw = Draw::get()->first();
+
+        $this->jackpot_value = $draw->jackpot_value;
+
+        //Calculo precio de las rifas (1% del jackpot/pozo acumulado) 
+        $this->raffles_price = round($this->jackpot_value*0.01, 2);
+
+        //Consulto Valor Dólar: 
+        $this->dollar_value = $draw->dollar;
+        
+        // //Consulto Valor Dólar
+        // $consulta_principal = Http::get("https://www.dolarsi.com/api/api.php?type=valoresprincipales");
+
+        //$banco_central = Http::withToken('eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTA2Njc3NjgsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJicmlhbmVwZXJpbGxvQGdtYWlsLmNvbSJ9.zEIxqkFOJAXtg5nahoY0xxhiPpzH8B0V9vB-dXQjjDLqHeZZmPpSB2dmFLy6P6pj1hEHT9hod7gK-VOUikc1rg')->get('https://api.estadisticasbcra.com/usd_of');
+
+        return view('livewire.raffles-info', ['dollar_value', 'jackpot_value', 'raffles_price']);
+
     }
+
 }
